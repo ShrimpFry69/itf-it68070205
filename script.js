8e837dc065027b5a8affb084c270c27e7b2f40d2
ShrimpFry69 <166158087+ShrimpFry69@users.noreply.github.com>
2025-10-03 15:41:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const operation = document.getElementById("operation")
+const amount = document.getElementById("amount")
+function change() {
+    cashbalance = document.getElementById("cashbalance")
+    accountbalance = document.getElementById("accountbalance")
+}
+
+function proceed() {
+    if (operation == "deposit"){
+        cashbalance
+    }
+}
