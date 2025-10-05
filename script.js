04bd91573e319dd9cc85c96d403b893ee82f7b8a
ShrimpFry69 <Aiyarat9922@gmail.com>
2025-10-05 13:35:41 +0700

    Made the website functional

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const operation = document.getElementById("operation")
-const amount = document.getElementById("amount")
+let count = 1;
+let cashbalance = 0;
+let = accountbalance = 0;
+
 function change() {
-    cashbalance = document.getElementById("cashbalance")
-    accountbalance = document.getElementById("accountbalance")
+    cashbalance = Number(document.getElementById("cashbalance").value)
+    accountbalance = Number(document.getElementById("accountbalance").value)
+    addLog("Balance Set.")
+}
+function addLog(message = '') {
+    const log = document.getElementById("log");
+    const entry = `${count} | ${message} | Account: ${accountbalance}, Cash: ${cashbalance}`;
+    log.innerText += entry + '\n';
+    count++;
 }
 
 function proceed() {
-    if (operation == "deposit"){
-        cashbalance
+    const operation = document.getElementById("operation").value
+    let amount = Number(document.getElementById("amount").value)
+    if (operation === "deposit"){
+        if (amount <= cashbalance) {
+            cashbalance = cashbalance - amount
+            accountbalance = accountbalance + amount
+            addLog(`Successfully Deposited ${amount}`)
+        }
+        else{
+            addLog("Not enough cash to deposit.")
+        }
+    }
+    else if(operation === "withdraw"){
+        if (amount <= accountbalance) {
+            cashbalance = cashbalance + amount
+            accountbalance = accountbalance - amount
+            addLog(`Successfully Withdrawed ${amount}`)
+        }
+        else{
+            addLog("Not enough cash to withdraw.")
+        }
     }
 }
